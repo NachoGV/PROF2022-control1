2292b1834c5c89704a729f204ac18f583da4a2c3
NachoGV <nachonio99@gmail.com>
2022-11-07 18:24:53 +0100

    Initial commit
    Creación del proyecto
    Arquetipo: maven-archetype-simple

---
+++ control1/src/main/java/com/control1/ignacio/App.java
@@ -0,0 +1,13 @@
+package com.control1.ignacio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
